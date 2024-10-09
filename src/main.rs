45126874f85429efe3da4ae7ef66cc5f70315fd2
truthixify <truthixify@gmail.com>
2024-10-09 04:58:18 +0100

    forgot to commit, now making a commit after a lot has been committed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::{configuration::get_configuration, run};
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener =
+        std::net::TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
---
+++ src/routes/mod.rs
@@ -0,0 +1,7 @@
+//! src/routes/mod.rs
+
+mod health_check;
+mod subscriptions;
+
+pub use health_check::*;
+pub use subscriptions::*;
